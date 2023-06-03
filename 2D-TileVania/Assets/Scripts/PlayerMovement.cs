ed237187204850c8ab74223162d88fbb8a6aa169
redblacktree <dustinrasener@gmail.com>
2023-06-03 07:47:47 -0400

    Tilevania - Lecture 83

--- 2D-TileVania/Assets/Scripts/PlayerMovement.cs
+++ 2D-TileVania/Assets/Scripts/PlayerMovement.cs
@@ -8,25 +8,54 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 5f;
 
     Vector2 moveInput;
-    Rigidbody2D rb;    
+    Rigidbody2D rb;
+    Animator animator;
+    struct PlayerState
+    {
+        public bool isRunning;
+        public bool isJumping;
+        public bool isFalling;
+        public bool isAttacking;
+        public bool isDead;
+    }
+    PlayerState playerState;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
-    }
-    
-    void Start()
-    {
-        
+        animator = GetComponent<Animator>();
+        PlayerState playerState = new PlayerState();
     }
 
     void Update()
     {
+        UpdateState();
         Run();
+        Animate();
+    }
+
+    void UpdateState()
+    {
+        playerState.isRunning = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;;
+    }
+
+    void Animate()
+    {
+        FlipSprite();
+        animator.SetBool("IsRunning", playerState.isRunning);
     }
 
     void Run() 
     {
         rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
+        animator.SetBool("IsRunning", playerState.isRunning);
+    }
+
+    void FlipSprite()
+    {
+        if (playerState.isRunning)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(moveInput.x), 1);
+        }
     }
 
     void OnMove(InputValue value)
