0ccab46b297f7bb59c4ddccac3ef533a3c19c8c1
redblacktree <dustinrasener@gmail.com>
2023-06-06 12:17:47 -0400

    Prevent coin double collecting

--- 2D-TileVania/Assets/Scripts/CoinPickup.cs
+++ 2D-TileVania/Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
-    
+    bool isCollected = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !isCollected)
         {
+            isCollected = true;
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             FindObjectOfType<GameSession>().AddCoin();
             Destroy(gameObject);
