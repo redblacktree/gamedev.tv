9ce08c76f2b8896990aaceb4e5d7e553cfca8e83
redblacktree <dustinrasener@gmail.com>
2023-06-12 08:01:33 -0400

    RPG-Combat System - Add Sprites, collisions, movement, animation - Lecture 11

--- 2D-RPG-Combat-System/Assets/Scripts/PlayerController.cs
+++ 2D-RPG-Combat-System/Assets/Scripts/PlayerController.cs
@@ -5,4 +5,67 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();        
+    }
+
+    private void FixedUpdate() {
+        
+        Move();
+        AdjustPlayerFacingDirection();
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + moveInput * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void PlayerInput()
+    {
+        moveInput = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        anim.SetFloat("moveX", moveInput.x);
+        anim.SetFloat("moveY", moveInput.y);
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 playerPos = transform.position;
+        
+        if (mousePos.x < playerPos.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (mousePos.x > playerPos.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
 }
