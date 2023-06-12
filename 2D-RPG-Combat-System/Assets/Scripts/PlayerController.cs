b4c192b210b2b72c94359895c8ccdbb76e368331
redblacktree <dustinrasener@gmail.com>
2023-06-11 23:23:55 -0400

    RPG-Combat System - Basic setup

---
+++ 2D-RPG-Combat-System/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+}
